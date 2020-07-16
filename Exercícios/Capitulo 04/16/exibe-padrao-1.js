e588a1f3cfe669a499439b5bea680048a64222b8
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-16 17:49:54 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Exercícios/Capitulo 04/16/exibe-padrao-1.js
@@ -0,0 +1,8 @@
+for (var contador = 1; contador <= 100; contador++){
+    var resto = contador % 2;
+    if (resto == 1){
+        console.log("*");
+    } else {
+        console.log("**");
+    }
+}
---
+++ Exercícios/Capitulo 04/19/exibe-padrao-2.js
@@ -0,0 +1,8 @@
+for (var contador = 1; contador <= 100; contador++){
+    var resto = contador % 4;
+    if (resto == 0){
+        console.log("PIN");
+    } else {
+        console.log(contador)
+    }
+}
