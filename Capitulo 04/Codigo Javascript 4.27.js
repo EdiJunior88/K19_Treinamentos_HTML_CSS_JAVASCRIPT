7b32b5d115c18e16b51d173100cdd3aeb24c3f43
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-16 17:41:06 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Capitulo 04/Codigo Javascript 4.27.js
@@ -0,0 +1,8 @@
+//While
+
+var contador = 0;
+
+while (contador < 100){
+    console.log("Bom Dia");
+    contador++;
+}
---
+++ Exercícios/Capitulo 04/13/exibe-1-ate-100.js
@@ -0,0 +1,3 @@
+for (var contador = 0; contador < 100; contador++){
+    console.log(contador)
+}
