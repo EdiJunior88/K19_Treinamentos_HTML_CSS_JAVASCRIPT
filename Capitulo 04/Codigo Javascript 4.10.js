23392f182df2791a2ff8ccf408490ecf5c7fb8c4
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-16 15:28:31 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Capitulo 04/Codigo Javascript 4.10.js
@@ -0,0 +1,25 @@
+// Exemplo de uso dos operadores de atribuição
+
+var valor = 1;
+//valor = 1
+
+valor += 2;
+//valor = 3
+
+valor -= 1;
+//valor = 2
+
+valor *= 6;
+//valor = 12
+
+valor /= 3;
+//valor = 4
+
+valor %= 3;
+//valor = 1
+
+valor++;
+//valor = 2
+
+valor--;
+//valor = 1
---
+++ Capitulo 04/Codigo Javascript 4.11.js
@@ -0,0 +1,25 @@
+// O mesmo exemplo anterior, usando os operadores aritméticos
+
+var valor = 1;
+//valor = 1
+
+valor = valor + 2;
+//valor = 3
+
+valor = valor - 1;
+//valor = 2
+
+valor = valor *  6;
+//valor = 12
+
+valor = valor / 3;
+//valor = 4
+
+valor = valor % 3;
+//valor = 1
+
+valor = valor + 1;
+//valor = 2
+
+valor = valor - 1;
+//valor = 1
