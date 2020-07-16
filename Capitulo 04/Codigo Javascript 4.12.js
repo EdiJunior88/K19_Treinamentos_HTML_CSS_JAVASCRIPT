36cec442119ccfe29c7625c5d75b7de84cdcc660
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-16 15:39:49 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Capitulo 04/Codigo Javascript 4.12.js
@@ -0,0 +1,11 @@
+//Exemplo de uso dos operadores relacionais em JavaScript
+
+var valor = 2;
+var t = false;
+
+t = (valor == 2); //t = true
+t = (valor != 2); //t = false
+t = (valor < 2);  //t = false
+t = (valor <= 2); //t = true
+t = (valor > 2);  //t = true
+t = (valor >= 2); //t = true
---
+++ Capitulo 04/Codigo Javascript 4.13.js
@@ -0,0 +1,6 @@
+//Exemplo de uso do operador &
+
+var a = Math.random();
+var b = Math.random();
+
+alert(a > 0.2 && b < 0.8);
