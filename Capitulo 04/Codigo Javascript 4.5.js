45116b56e73871c7b176646ae0f04884f78d8047
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-16 13:47:32 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Capitulo 04/Codigo Javascript 4.5.js
@@ -0,0 +1,38 @@
+//Exemplo de uso dos operadores aritméticos
+
+var umMaisUm = 1 + 1;
+//umMaisUm = 2
+
+var tresVezesDois = 3 * 2;
+//tresVezesDois = 6
+
+var quatroDivididoPorDois = 4 / 2;
+//quatroDivididoPorDois = 2
+
+var seisModuloCindo = 6 % 5;
+//seisModuloCinco = 1
+
+var x = 7;
+
+x = x + 1 * 2;
+//x = 9
+
+x = x - 3;
+//x = 6
+
+x = x / (6 - 2 - (3 * 5) / (16 - 1));
+//x = 2
+
+/* O módulo de um número x, na matemática, é o valor numérico de x
+desconsiderando o seu sinal (valor absoluto). Na matemática expressamos
+o módulo da seguinte forma: |-2| = 2.
+Em linguagens de programação, o módulo de um número é o resto da divisão
+desse número por outro. No exemplo acima, o resto da divisão de 6 por 5
+é igual a 1. Além disso, lemos a expressão 6 % 5 da seguinte forma: seis
+módulo cinco. */
+
+/* As operações aritméticas em JavaScript obedecem as mesmas regras da
+matemática com relação à precedência dos operadores e parênteses. Portanto,
+as operações são resolvidas a partir dos parênteses mais internos até os
+mais externos, primeiro resolvemos as multiplicações, divisões e os módulos.
+Em seguida, resolvemos as adições e subtrações. */
---
+++ Capitulo 04/Codigo Javascript 4.6.js
@@ -0,0 +1,10 @@
+//Potenciação e raiz quadrada
+
+/* As operações de potenciação e raiz quadrada podem ser realizadas através
+dos métodos Math.pow e Math.sqrt. Veja alguns exemplos. */
+
+var a = Math.pow(3, 5);
+//a = 243
+
+var b = Math.sqrt(9);
+//b = 3
