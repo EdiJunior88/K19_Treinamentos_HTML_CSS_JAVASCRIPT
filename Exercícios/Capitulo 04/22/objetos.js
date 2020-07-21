74f6d0383e93e6c878cee36225671285b0bfed96
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-21 14:13:26 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Exercícios/Capitulo 04/22/objetos.js
@@ -0,0 +1,15 @@
+var curso = {
+    sigla: "K11",
+    nome: "Orientação a Objetos em Java"
+};
+
+console.log(curso.sigla);
+console.log(curso.nome);
+
+var curso2 = {
+    sigla: "K12",
+    nome: "Desenvolvimento Web com JSF2 e JPA2"
+};
+
+console.log(curso2.sigla);
+console.log(curso2.nome);
---
+++ Exercícios/Capitulo 04/25/referencias.js
@@ -0,0 +1,10 @@
+var curso = {
+    sigla: "K11",
+    nome: "Orientação a Objetos em JAVA"
+};
+
+//exibe K11
+console.log(curso.sigla);
+
+//exibe Orientação a Objetos em JAVA
+console.log(curso.nome);
