79494ae7403f946a1d123719321623602a39bd38
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-28 14:24:05 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Capitulo 04/arrays.js
@@ -0,0 +1,16 @@
+//A linguagem Javascript provê um objeto com características semelhantes a um array. Esse tipo de objeto pode ser criado de forma literal com a sintaxe JSON
+
+var vazio = [];
+var cursos = ["K01", "K02", "K03"];
+
+console.log(vazio[0]);
+//undefined
+
+console.log(cursos[0]);
+//K01
+
+console.log(vazio.length);
+//0
+
+console.log(cursos.length);
+//3
---
+++ Capitulo 04/arrays_2.js
@@ -0,0 +1,8 @@
+//Percorrendo um Array
+//Para percorrer um array, podemos utilizar o comando for.
+
+var cursos = ["K01", "K02", "K03"];
+
+for (var i = 0; i < cursos.length; i++){
+    console.log(cursos[i]);
+}
