ab57f0705f1f215f66677f4e0a30e4b4465cc14a
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-30 15:44:45 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Exercícios/Capitulo 04/52/array-length.js
@@ -0,0 +1,8 @@
+var vazio = []
+var cursos = ["K11", "K12", "K21", "K22", "K23", "K31", "K32"];
+
+console.log(vazio[0]);
+console.log(cursos[0]);
+
+console.log(vazio.length);
+console.log(cursos.length);
---
+++ Exercícios/Capitulo 04/55/array-elementos.js
@@ -0,0 +1,5 @@
+var cursos = ["K11", "K12", "K21", "K22", "K23", "K31", "K32"];
+
+for (var i = 0; i < cursos.length; i++){
+    console.log(cursos[i]);
+}
