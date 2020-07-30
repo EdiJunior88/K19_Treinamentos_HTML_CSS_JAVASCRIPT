0b12b660ca557f75f5fe15f5e2765d3053056302
EdiJunior88 <er.moura88@yahoo.com.br>
2020-07-30 15:35:32 -0300

    K19 - Desenvolvimento Web com HTML5, CSS e JavaScript

---
+++ Capitulo 04/strings.js
@@ -0,0 +1,6 @@
+//Acessando os Caracteres de uma String por Posição
+//O método charAt() retorna o caractere na posição especificada.
+
+var curso = "K12";
+
+console.log(curso.charAt(0))
---
+++ Capitulo 04/strings_2.js
@@ -0,0 +1,6 @@
+//Recuperando um Trecho de uma String
+//O método slice() retorna uma porção de uma string.
+
+var curso = "K12 - Desenvolvimento Web com JSF2 e JPA2";
+
+console.log(curso.slice(0,3));
